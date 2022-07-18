825993980df444d4afc57ab4cc76eec66d2877b7
Ruan Oliveira <ruanamaral40@gmail.com>
2022-07-18 19:07:23 -0300

    tela home mobile finalizada

--- assets/scripts/lateral_menu.js
+++ assets/scripts/lateral_menu.js
@@ -1,6 +1,12 @@
 const menuButton = document.querySelector('.header__menu');
 const lateralMenu = document.querySelector('.lateral__menu')
+const html = document.documentElement;
 
 menuButton.addEventListener('click', function () {
     lateralMenu.classList.toggle('active__menu')
+    html.classList.toggle('lock__menu');
 })
+
+
+
+
